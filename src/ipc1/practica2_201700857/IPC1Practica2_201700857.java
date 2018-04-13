279d299ca74539f86acc5a45fd1a68b5f81923d7
Bminas <Bminas@Dalfaro117>
2018-04-12 22:53:14 -0600

    Main y ya :v

---
+++ src/ipc1/practica2_201700857/IPC1Practica2_201700857.java
@@ -0,0 +1,12 @@
+package ipc1.practica2_201700857;
+
+
+public class IPC1Practica2_201700857 {
+
+
+    public static void main(String[] args) {
+//        NaveEnemiga_201700857 n1 = new NaveEnemiga_201700857();
+            Espacio_201700857 juego = new Espacio_201700857();
+    }
+    
+}
