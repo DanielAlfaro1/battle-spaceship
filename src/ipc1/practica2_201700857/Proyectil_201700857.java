ee072e428ba810e5e434d99ed3dcc6e80038423a
Bminas <Bminas@Dalfaro117>
2018-04-12 22:52:07 -0600

    Esto se quedó de momento parado pero será utilizado luego

---
+++ src/ipc1/practica2_201700857/Proyectil_201700857.java
@@ -0,0 +1,24 @@
+package ipc1.practica2_201700857;
+
+
+public class Proyectil_201700857  {
+    Thread t;
+    String TipoDeBala;
+
+//    public Proyectil_201700857(String nombre){
+//        this.TipoDeBala = nombre;
+//        t = new Thread (this,"TipoDeBala");
+//        t.start();
+//    }
+//    
+//    @Override
+//    public void run() {
+//    try{
+//        for(int i=0;i<Espacio_201700857.Balas.length;i++){
+//            
+//        }
+//    }catch(InterruptedException e){};
+//    }
+    
+    
+}
