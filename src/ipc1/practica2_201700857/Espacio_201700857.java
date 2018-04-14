91f4e692de472f63848b4fc0fc46e72e54152bb3
Bminas <Bminas@Dalfaro117>
2018-04-14 00:23:54 -0600

    Agregados a la pantalla principal

--- src/ipc1/practica2_201700857/Espacio_201700857.java
+++ src/ipc1/practica2_201700857/Espacio_201700857.java
@@ -9,19 +9,22 @@ import java.util.ArrayList;
 
 public class Espacio_201700857 {
 JFrame V1;
-JPanel Espacio;
-JLabel Tiempo,fondo,nave,naveEscudo;
+JPanel Espacio,Stats;
+public static JLabel Tiempo,fondo,nave,naveEscudo,Contador,Puntos,letra,ContPuntos,Segundos,Life,PorcLife,dest,ContDest;
 JButton Arriba, Abajo;
+Contador_201700857 timer;
 NaveEnemiga_201700857 n;
 public static JLabel[] Enemigos = new JLabel[50];
 public static JLabel[] Balas = new JLabel[30];
+public static NaveEnemiga_201700857 prueba;
 NaveEnemiga_201700857 n1;
 ArrayList componentes;
 int up=0,down=0,left=0,right=0,b=0,a=0,posi=0;
-int x= 550;
-int y = 200;
-public static int[][] coor= new int[30][2];
-public static int pruebay;
+public static int v=0;
+public static int x= 550;
+public static int y = 200;
+public static int[][] coor= new int[50][2];
+public static int pruebay,destroy=0,vida=100,punch=0;
 public static Traslacion_201700857[] probamos = new Traslacion_201700857[30];
 
 //public void Ship(){
@@ -32,25 +35,62 @@ public static Traslacion_201700857[] probamos = new Traslacion_201700857[30];
 //        fondo.add(Enemigos[i]);
 //    }
 //}
-
-public void Arriba(NaveEnemiga_201700857 n){
-    Espacio.add(n.enemigo);
+public void Escudo(int visi){
+    
+    if (visi>=3){
+        nave.setVisible(false);
+        naveEscudo.setVisible(true);
+        v=1;
+        visi=0;
+    }
+    
+}
+public void NoEscudo(){
+    if(Traslacion_201700857.hits>=3){
+        naveEscudo.setVisible(false);
+        nave.setVisible(true);
+        v=0;
+        Traslacion_201700857.hits=0;
+    }
 }
 
-public void añadir(int n){
-    Enemigos[n].setBounds(0,0,60,50);
-    fondo.add(Enemigos[n]);
+public void choque(){
+    nave.setVisible(false);
+    naveEscudo.setVisible(true);
 }
 
 public void CrearPanel(){
     //Paneles
     Espacio = new JPanel();
     Espacio.setLayout(null);
+    Stats = new JPanel();
+    Stats.setLayout(null);
+    Stats.setBounds(700,0,150,500);
         
     //Labels
     fondo = new JLabel();
     fondo.setIcon(new ImageIcon("img/Espacio.png"));
     fondo.setBounds(0,0,700,500);
+    Contador = new JLabel("00");
+    Contador.setBounds(50,15,75,50);
+    Segundos = new JLabel("SEGUNDOS");
+    Segundos.setBounds(50,65,75,25);
+    Puntos = new JLabel("PUNTOS");
+    Puntos.setBounds(50,200,75,20);
+    letra = new JLabel("s");
+    letra.setBounds(130,15,20,20);
+    ContPuntos = new JLabel("00");
+    ContPuntos.setBounds(50,140,75,50);
+    Life = new JLabel("PORCENTAJE DE VIDA");
+    Life.setBounds(50,320,75,20);
+    PorcLife = new JLabel("100%");
+    PorcLife.setBounds(50,240,75,50);
+    dest  = new JLabel("NAVES DESTRUIDAS");
+    dest.setBounds(50,395,75,50);
+    ContDest = new JLabel();
+    ContDest.setBounds(50,315,75,50);
+    Tiempo = new JLabel("00");
+    Tiempo.setBounds(50,450,75,50);
     //Nave
     nave = new JLabel();
     nave.setBounds(x,y,100,75);
@@ -58,7 +98,7 @@ public void CrearPanel(){
     
     naveEscudo = new JLabel();
     naveEscudo.setBounds(x,y,100,75);
-    naveEscudo.setIcon(new ImageIcon("img/fragataShield2.png"));
+    naveEscudo.setIcon(new ImageIcon("img/fragataescudo.png"));
     naveEscudo.setVisible(false);
    // Boton
    Arriba = new JButton("ARRIBA");
@@ -69,11 +109,17 @@ public void CrearPanel(){
    Abajo.addMouseListener(accion);
    //Agegar objetos
    Espacio.add(fondo);
-//   Espacio.add(Arriba);
-//   Espacio.add(Abajo);
-//   Espacio.add(nave);
-//   fondo.add(Arriba);
-//   fondo.add(Abajo);
+   Espacio.add(Stats);
+//   Stats.add(Tiempo);
+   Stats.add(Contador);
+   Stats.add(letra);
+   Stats.add(Segundos);
+   Stats.add(Puntos);
+   Stats.add(ContPuntos);
+   Stats.add(Life);
+   Stats.add(PorcLife);
+   Stats.add(dest);
+   Stats.add(ContDest);
    fondo.add(nave);
    fondo.add(naveEscudo);
    Espacio.addKeyListener(teclado);
@@ -97,16 +143,19 @@ public void CrearPanel(){
 }
 public void CrearVentana(){
     V1 = new JFrame("MINI-GALAGA");
-    V1.setBounds(150,150,700,500);
+    V1.setBounds(150,150,850,500);
     V1.add(Espacio);
     V1.setVisible(true);
     V1.setDefaultCloseOperation(V1.EXIT_ON_CLOSE);
     V1.addKeyListener(teclado);
+    
 }
 public Espacio_201700857(){
 //    Ship();
+    
     CrearPanel();
     CrearVentana();
+    timer = new Contador_201700857();
 }
 
 KeyListener teclado = new KeyListener(){
@@ -162,8 +211,10 @@ KeyListener teclado = new KeyListener(){
                 right=0;
                 b=0;
                 a=0;
-                nave.setVisible(false);
-                naveEscudo.setVisible(true);
+                int m=3;
+                Escudo(m);
+//                nave.setVisible(false);
+//                naveEscudo.setVisible(true);
                 
             }else{
                 up=0;
@@ -174,12 +225,16 @@ KeyListener teclado = new KeyListener(){
                 a=0;
             }        
     }
+        
+        
         if(e.getKeyCode()==KeyEvent.VK_R){
-            nave.setVisible(true);
-                naveEscudo.setVisible(false);
+//            nave.setVisible(true);
+//                naveEscudo.setVisible(false);
+            int aqui =3;
+                NoEscudo();
         }
         if(e.getKeyCode()==KeyEvent.VK_P){
-            NaveEnemiga_201700857 prueba = new NaveEnemiga_201700857("prueba");
+            prueba = new NaveEnemiga_201700857("prueba");
         }
         if(e.getKeyCode()==KeyEvent.VK_SPACE){
             if(posi==Balas.length){
@@ -212,7 +267,7 @@ MouseListener accion = new MouseListener(){
 //            }
 //            nave.setVisible(true);
 //            naveEscudo.setVisible(false);
-        añadir(1);
+//        añadir(1);
         }
         if (e.getSource()==Abajo){
             if(y<=375){
