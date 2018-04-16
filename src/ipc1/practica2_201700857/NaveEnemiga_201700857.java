98cc6d0997297e6b9e9731d34e5a21a272d2f842
Bminas <Bminas@Dalfaro117>
2018-04-15 20:10:20 -0600

    Cambios en movimientos

--- src/ipc1/practica2_201700857/NaveEnemiga_201700857.java
+++ src/ipc1/practica2_201700857/NaveEnemiga_201700857.java
@@ -12,9 +12,6 @@ public class NaveEnemiga_201700857 implements Runnable {
     public static int[] impac = new int[50];
     
     public NaveEnemiga_201700857(){
-//        t = new Thread(this,"Nave");
-//        t.start();
-//    enemigo = new JLabel();
     enemigo.setBounds(0,0,60,50);
     enemigo.setIcon(new ImageIcon("img/phantom"));
     enemigo.setVisible(true);
@@ -31,25 +28,18 @@ public class NaveEnemiga_201700857 implements Runnable {
     public void stop(){
         t.suspend();
     }
-//    public NaveEnemiga_201700857(JLabel[] ship){
-////        
-//    }
     public void run(){
         try{
             for(int i=0;i<Espacio_201700857.Enemigos.length;i++){
                 int y = 0;
-                int ran = (int)(Math.random()*(3000-2500+1)+2500);
+                int ran = (int)(Math.random()*(1500-1200+1)+1200);
                 y = (int)(Math.random()*420)+10;
                 Espacio_201700857.Enemigos[i].setLocation(0, y);
-                System.out.println(nave+" - valor: "+i);
+//                System.out.println(nave+" - valor: "+i);
                 NaveEnemiga_201700857.impac[i]=0;
-//                System.out.println(ran);
                 tr = new Traslacion_201700857(y,i,1);
             Thread.sleep(ran);
             }
-//            enemigo.setBounds(0,0,60,50);
-//            enemigo.setIcon(new ImageIcon("img/phantom"));
-//            enemigo.setVisible(true);
         }catch (InterruptedException e){};
     }
 }
